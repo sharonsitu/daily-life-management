275e7c72ad2012c604f699f938ae2c6869a04b67
sharonsitu <ssworld1120@gmail.com>
2019-12-18 11:23:08 -0500

    add user backend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const express = require("express");
 var cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-const Data = require("./data");
+const User = require("./models/user");
+const bcrypt = require("bcrypt-nodejs");
 
 const API_PORT = 3001;
 const app = express();
@@ -12,7 +13,7 @@ const router = express.Router();
 
 // this is our MongoDB database
 const dbRoute =
-  "mongodb+srv://<userName>:<password>@dailylife-x3bmh.mongodb.net/test?retryWrites=true&w=majority";
+  "mongodb+srv://{userName}:{password}@dailylife-x3bmh.mongodb.net/test?retryWrites=true&w=majority";
 
 // connects our back end code with the database
 mongoose.connect(dbRoute, { useNewUrlParser: true });
@@ -30,51 +31,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
-// this is our get method
-// this method fetches all available data in our database
-router.get("/getData", (req, res) => {
-  Data.find((err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: data });
-  });
+app.get("/", function(req, res) {
+  res.send("hello world");
 });
-
-// this is our update method
-// this method overwrites existing data in our database
-router.post("/updateData", (req, res) => {
-  const { id, update } = req.body;
-  Data.findByIdAndUpdate(id, update, err => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
-
-// this is our delete method
-// this method removes existing data in our database
-router.delete("/deleteData", (req, res) => {
-  const { id } = req.body;
-  Data.findByIdAndRemove(id, err => {
-    if (err) return res.send(err);
-    return res.json({ success: true });
-  });
-});
-
 // this is our create methid
 // this method adds new data in our database
-router.post("/putData", (req, res) => {
-  let data = new Data();
-
-  const { id, message } = req.body;
-
-  if ((!id && id !== 0) || !message) {
-    return res.json({
-      success: false,
-      error: "INVALID INPUTS"
-    });
-  }
-  data.message = message;
-  data.id = id;
-  data.save(err => {
+router.post("/createUser", (req, res) => {
+  const user = new User();
+  const { email, password } = req.body;
+  user.email = email;
+  user.password = password;
+  console.log(user);
+  user.save(err => {
     if (err) return res.json({ success: false, error: err });
     return res.json({ success: true });
   });
