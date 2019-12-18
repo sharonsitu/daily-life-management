45f20505a76c9f91f64351e5aaa8acda146ef8d0
sharonsitu <ssworld1120@gmail.com>
2019-12-18 12:34:59 -0500

    hash password

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ var cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const User = require("./models/user");
-const bcrypt = require("bcrypt-nodejs");
+const bcrypt = require("bcrypt");
 
 const API_PORT = 3001;
 const app = express();
@@ -34,22 +34,35 @@ app.use(logger("dev"));
 app.get("/", function(req, res) {
   res.send("hello world");
 });
-// this is our create methid
-// this method adds new data in our database
-router.post("/createUser", (req, res) => {
-  const user = new User();
-  const { email, password } = req.body;
-  user.email = email;
-  user.password = password;
-  console.log(user);
-  user.save(err => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
 
 // append /api for our http requests
 app.use("/api", router);
+
+router.post("/createUser", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email: email })
+    .then(user => {
+      if (user) {
+        return res.json({ error: "User exists already" });
+      }
+      return bcrypt.hash(password, 12);
+    })
+    .then(hashedPassword => {
+      const user = new User({
+        email,
+        password: hashedPassword
+      });
+      return user.save();
+    })
+    .then(result => {
+      return res.json({ success: true });
+    })
+    .catch(err => {
+      console.log(err);
+      return res.json({ status: 500 });
+      throw err;
+    });
+});
 
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
